87b9c08d27a632df4c250f1b9277c3bb4526e706
Ferit <ferit@cryptolab.net>
2024-08-21 17:26:54 +0100

    refactor(project): upgrade

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,7 +22,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: {
-    version: "0.8.10",
+    version: "0.8.26",
     settings: {
       optimizer: {
         enabled: true,
